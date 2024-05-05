4ad51903472934aa81fa7d79063519d93074d9bf
MuthuAjay <muthuajay97@gmail.com>
2024-05-05 18:38:32 +0530

    updated main

---
+++ makemore_module/main.py
@@ -0,0 +1,85 @@
+import torch
+from activations import Tanh, BatchNorm1d
+from plot import Plot
+from typing import List
+from model import Linear
+from dataset import Dataset
+from train import Train
+
+g = torch.Generator().manual_seed(42)
+
+
+class Main:
+
+    def __init__(self,
+                 n_embd: int = 10,
+                 n_hidden: int = 100
+                 ):
+        self.n_embd = n_embd
+        self.n_hidden = n_hidden
+        self.g = torch.Generator().manual_seed(42)
+
+    def instantiate_embeddings(self, vocab_size, n_embd):
+        return torch.randn((vocab_size, n_embd), generator=g)
+
+    def instantiate_layers(self, block_size, vocab_size):
+        layers = [
+            Linear(self.n_embd * block_size, self.n_hidden), Tanh(),
+            Linear(self.n_hidden, self.n_hidden), Tanh(),
+            Linear(self.n_hidden, self.n_hidden), Tanh(),
+            Linear(self.n_hidden, self.n_hidden), Tanh(),
+            Linear(self.n_hidden, self.n_hidden), Tanh(),
+            Linear(self.n_hidden, vocab_size)
+        ]
+        return layers
+
+    def initialize_weights(self,
+                           layers: List[object],
+                           final_layer_weights: float = 0.1,
+                           other_weights: float = 5 / 3):
+        with torch.no_grad():
+            # last layer : make less confident
+            layers[-1].weight *= final_layer_weights
+            # all other layers: apply again
+            for layer in layers[:-1]:
+                if isinstance(layer, Linear):
+                    layer.weight *= other_weights
+
+    def parameters(self,
+                   layers: List[object]) -> List:
+        # parameters for back propagation
+
+        parameters = [self.C] + [p for layer in layers for p in layer.parameters()]
+        print(sum(p.nelement() for p in parameters))  # number of parameters in total
+        for p in parameters:
+            p.requires_grad = True
+
+        return parameters
+
+    def __call__(self,
+                 block_size: int = 3):
+        dataset = Dataset(path=r'C:\Users\CD138JR\Documents\GitHub\gpt\names.txt',
+                          block_size=3)
+        Xtr, Ytr, Xdev, Ydev, Xte, Yte = dataset.get_dataset()
+        vocab_size = dataset.vocab_size
+        self.C = self.instantiate_embeddings(vocab_size=vocab_size,
+                                             n_embd=self.n_embd)
+        layers = self.instantiate_layers(block_size=3,
+                                         vocab_size=vocab_size)
+        parameters = self.parameters(layers=layers)
+        train = Train(batch_size=32,
+                      lr=0.1,
+                      max_steps=10)
+        layers, parameters, lossi, ud = train.train(Xtr=Xtr,
+                                                   Ytr=Ytr,
+                                                   parameters=parameters,
+                                                   C=self.C,
+                                                   layers=layers)
+        Plot.plot_activation_distribution(layers=layers,
+                                          act_fn=Tanh)
+
+
+if __name__ == "__main__":
+    main = Main(n_embd= 10,
+                n_hidden=100)
+    main()
