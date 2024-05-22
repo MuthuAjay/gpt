3690a106f1d3e003008f2db78ac14c36c5042bce
MuthuAjay <muthuajay97@gmail.com>
2024-05-23 01:55:49 +0530

    updated model initialization

--- makemore_wavenet_module/main.py
+++ makemore_wavenet_module/main.py
@@ -1,9 +1,10 @@
-import torch
-from plot import Plot
 from typing import List
+
+import torch
+
 from dataset import Dataset
-from train import Train
 from layers import Embedding, Sequential, Linear, FlattenConsecutive, Tanh, BatchNorm1d
+from train import Train
 
 g = torch.Generator().manual_seed(42)
 
@@ -13,30 +14,64 @@ class Main:
     def __init__(self,
                  n_embd: int = 10,
                  n_hidden: int = 100,
-                 add_norm: bool = False,
                  ):
         self.n_embd = n_embd
         self.n_hidden = n_hidden
         self.g = torch.Generator().manual_seed(42)
-        self.add_norm = add_norm
 
     def instantiate_model(self,
                           block_size: int,
                           vocab_size: int,
                           ):
-        model = Sequential([
-            Embedding(vocab_size, self.n_embd),
-            FlattenConsecutive(2), Linear(self.n_embd * 2, self.n_hidden, bias=False), BatchNorm1d(self.n_hidden),
-            Tanh(),
-            FlattenConsecutive(2), Linear(self.n_hidden * 2, self.n_hidden, bias=False), BatchNorm1d(self.n_hidden),
-            Tanh(),
-            FlattenConsecutive(2), Linear(self.n_hidden * 2, self.n_hidden, bias=False), BatchNorm1d(self.n_hidden),
-            Tanh(),
-            FlattenConsecutive(2), Linear(self.n_hidden * 2, self.n_hidden, bias=False), BatchNorm1d(self.n_hidden),
-            Tanh(),
-            Linear(self.n_hidden, vocab_size),
-        ])
-        return model
+
+        # model = Sequential([
+        #     Embedding(vocab_size, self.n_embd),
+        #     FlattenConsecutive(2), Linear(self.n_embd * 2, self.n_hidden, bias=False), BatchNorm1d(self.n_hidden),
+        #     Tanh(),
+        #     FlattenConsecutive(2), Linear(self.n_hidden * 2, self.n_hidden, bias=False), BatchNorm1d(self.n_hidden),
+        #     Tanh(),
+        #     FlattenConsecutive(2), Linear(self.n_hidden * 2, self.n_hidden, bias=False), BatchNorm1d(self.n_hidden),
+        #     Tanh(),
+        #     FlattenConsecutive(2), Linear(self.n_hidden * 2, self.n_hidden, bias=False), BatchNorm1d(self.n_hidden),
+        #     Tanh(),
+        #     Linear(self.n_hidden, vocab_size),
+        # ])
+
+        model1 = Sequential(
+            [Embedding(vocab_size, self.n_embd)] + self.initianlze_layers(block_size=block_size,
+                                                                          wavnet_dim=2) + [
+                Linear(self.n_hidden, vocab_size), ]
+        )
+        return model1
+
+    def _layers_count(self,
+                      block_size: int,
+                      wavenet_dim: int):
+        layer_count = 0
+        if wavenet_dim == 2:
+            while block_size > 0:
+                if block_size % 2 == 0:
+                    layer_count += 1
+                    block_size /= 2
+                else:
+                    block_size = 0
+        print(layer_count)
+        return layer_count
+
+    def initianlze_layers(self,
+                          block_size: int,
+                          wavnet_dim: int = 2):
+        layers = []
+        num_layers = self._layers_count(block_size, wavnet_dim)
+        for i in range(num_layers):
+            if i == 0:
+                layers = layers + [FlattenConsecutive(2), Linear(self.n_embd * 2, self.n_hidden, bias=False),
+                                   BatchNorm1d(self.n_hidden), Tanh()]
+            else:
+                layers += [FlattenConsecutive(2), Linear(self.n_hidden * 2, self.n_hidden, bias=False),
+                           BatchNorm1d(self.n_hidden), Tanh()]
+        print(len(layers))
+        return layers
 
     def initialize_weights(self,
                            model: Sequential):
@@ -62,11 +97,11 @@ class Main:
                  lr: float = 0.1,
                  ):
         dataset = Dataset(path=r'C:\Users\CD138JR\Documents\GitHub\gpt\names.txt',
-                          block_size=3)
+                          block_size=block_size)
         Xtr, Ytr, Xdev, Ydev, Xte, Yte = dataset.get_dataset()
         vocab_size = dataset.vocab_size
 
-        model = self.instantiate_model(block_size=3,
+        model = self.instantiate_model(block_size=block_size,
                                        vocab_size=vocab_size)
         model = self.initialize_weights(model)
         parameters = self.parameters(model=model)
@@ -76,31 +111,20 @@ class Main:
         layers, parameters, lossi, ud = train(Xtr=Xtr,
                                               Ytr=Ytr,
                                               parameters=parameters,
-                                              C=self.C,
                                               model=model)
-        Plot.plot_activation_distribution(layers=layers,
-                                          act_fn=Tanh)
-        Plot.plot_gradient_distribution(layers=layers,
-                                        act_fn=Tanh)
-        Plot.weights_gradient_distribution(parameters=parameters)
-        Plot.plot_updates_to_gradients_ratio(parameters=parameters,
-                                             ud=ud)
 
 
 if __name__ == "__main__":
     add_norm_values = [False, True]
     learning_rates = [0.1, 0.01]
-    weight_initializations = [[0.1, 1.1], [0.2, 1.2], [0.5, 2]]
 
-    for add_norm_val in add_norm_values:
-        for lr in learning_rates:
-            for weight_init in weight_initializations:
-                main = Main(n_embd=10,
-                            n_hidden=100,
-                            add_norm=add_norm_val)
-                train_settings = {
-                    'lr': lr,
-                    'weight_init': weight_init
-                }
-                print(f"Training with add_norm={add_norm_val}, lr={lr}, weight_init={weight_init}")
-                main(max_steps=1000, **train_settings)
+    main = Main(n_embd=10,
+                n_hidden=100)
+    # train_settings = {
+    #     'lr': lr,
+    #     'weight_init': weight_init
+    # }
+    main(
+        block_size=32,
+        max_steps=1000
+    )
